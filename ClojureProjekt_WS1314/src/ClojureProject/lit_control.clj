f8ea52ef3d581da197452122636e31cc2c174c40
anadler <alexander.nadler@mni.thm.de>
2014-01-12 14:39:50 +0100

    lit_control method formating

--- ClojureProjekt_WS1314/src/ClojureProject/lit_control.clj
+++ ClojureProjekt_WS1314/src/ClojureProject/lit_control.clj
@@ -8,47 +8,58 @@
 ; -------------------------------------------------------------------------------------
 ; DATABASE OPERATIONS
 
-(defn saveConfiguration 
+(defn saveConfiguration
+  ""
   [] ())
 
-(defn controlConnectDatabase [host db user password]  
-  (ClojureProject.lit_data/connectDatabase host db user password))
+(defn controlConnectDatabase 
+  ""
+  [host db user password] (ClojureProject.lit_data/connectDatabase host db user password))
 
-(defn controlDisconnectDatabase []  
-  (ClojureProject.lit_data/disconnectDatabase))
+(defn controlDisconnectDatabase 
+  ""
+  [] (ClojureProject.lit_data/disconnectDatabase))
 
 ; -------------------------------------------------------------------------------------
 ; SEARCH ITEMS
 
-(defn executeSearchTitle [searchVec]
-  (println (ClojureProject.lit_data/selectTitle)))
+(defn executeSearchTitle 
+  ""
+  [searchTitleVec] (println (ClojureProject.lit_data/selectTitle)))
 
-(defn executeSearchPublisher [searchVec]
-  (println (ClojureProject.lit_data/selectPublisher)))
+(defn executeSearchPublisher 
+  ""
+  [searchPublisherVec] (println (ClojureProject.lit_data/selectPublisher)))
 
 ; -------------------------------------------------------------------------------------
 ; ADD ITEMS
 
-(defn executeAddTitle [title]
-  (println title))
+(defn executeAddTitle 
+  ""
+  [title] (println title))
 
-(defn executeAddPublisher [publisher]
-  (println publisher))
+(defn executeAddPublisher 
+  ""
+  [publisher] (println publisher))
 
 ; -------------------------------------------------------------------------------------
 ; MODIFY ITEMS
 
-(defn executeModifyTitle [title]
-  (println title))
+(defn executeModifyTitle 
+  ""
+  [title] (println title))
 
-(defn executeModifyPublisher [publisher]
-  (println publisher))
+(defn executeModifyPublisher 
+  ""
+  [publisher] (println publisher))
 
 ; -------------------------------------------------------------------------------------
 ; DELETE ITEMS
 
-(defn executeDeleteTitle [title]
-  (println title))
+(defn executeDeleteTitle 
+  ""
+  [title] (println title))
 
-(defn executeDeletePublisher [publisher]
-  (println publisher))
+(defn executeDeletePublisher 
+  ""
+  [publisher] (println publisher))
