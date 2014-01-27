10f7931960c5e38c8af1a550994eb7edcc00a05b
Alexander Nadler <alexander.nadler@mni.thm.de>
2014-01-27 15:52:37 +0100

    added log4j.properties and log4j to project, added log info and debug messages to lit_data

--- ClojureProjekt_WS1314/src/ClojureProject/lit_data.clj
+++ ClojureProjekt_WS1314/src/ClojureProject/lit_data.clj
@@ -21,6 +21,7 @@
 (defn connectDatabase 
   "Opens the connection to a database with the given connection data."
   ([connectionData] (defdb mySQLDatabase (mysql connectionData)) 
+                    (log/info "connection opened to database [" mySQLDatabase "]")
                     
                     (defentity title
                       (database mySQLDatabase) 
@@ -28,16 +29,19 @@
                       (table :tbl_title)
                       (entity-fields :isbn :name :author :publisher_id)
                       (belongs-to publisher {:fk :publisher_id}))
+                    (log/info "defined entity title [" title "]")
 
                     (defentity publisher
                       (database mySQLDatabase) 
                       (pk :id) 
                       (table :tbl_publisher)
-                      (entity-fields :name))))               
+                      (entity-fields :name))
+                    (log/info "defined entity publisher [" publisher "]")))               
 
 (defn disconnectDatabase
   "Closes the connection to the actual defined database."
   [] 
+  (log/debug "close database connection")
   (-> (connection-pool (get-connection mySQLDatabase)):datasource .close))
 
 ; -------------------------------------------------------------------------------------
@@ -45,59 +49,75 @@
 
 (defn selectTitle
   "Selects a number of titles from the database"    
-  ([] (select title (order :name :ASC)))  
-  ([conditions] (select title 
-                        (where (and {:isbn [like (get conditions :isbn)]}
-                                    {:name [like (get conditions :name)]}
-                                    ;{:publisher_id [like (get conditions :publisher_id)]}
-                                    {:author [like (get conditions :author)]}))
-                        (order :name :ASC))))
+  ([] (let [result (select title (order :name :ASC))]
+        (log/debug "select all title result [" result "]") result))    
+  ([conditions] (let [result (select title 
+                                     (where (and {:isbn [like (get conditions :isbn)]}
+                                                 {:name [like (get conditions :name)]}
+                                                 ;{:publisher_id [like (get conditions :publisher_id)]}
+                                                 {:author [like (get conditions :author)]}))
+                                     (order :name :ASC))]
+                (log/debug "select title conditions [" conditions "]")
+                (log/debug "select title result [" result "]") result)))
 
 (defn selectPublisher
   "Selects a number of publishers from the database"  
-  ([] (select publisher (order :name :ASC)))  
-  ([conditions] (select publisher 
-                        (where {:name [like (get conditions :name)]}) 
-                        (order :name :ASC))))
+  ([] (let [result (select publisher (order :name :ASC))]
+         (log/debug "select all publisher result [" result "]")))    
+  ([conditions] (let [result (select publisher 
+                                     (where {:name [like (get conditions :name)]}) 
+                                     (order :name :ASC))]
+                (log/debug "select publisher conditions [" conditions "]")
+                (log/debug "select publisher result [" result "]"))))
 
 ; -------------------------------------------------------------------------------------
 ; DATABASE INSERT
 
 (defn insertTitle
   "Inserts a title in the database"
-  [newtitle] (insert title (values newtitle)))
+  ([newtitle] 
+    (log/debug "insert title [" newtitle "] into database")
+    (insert title (values newtitle))))
 
 (defn insertPublisher
   "Inserts a publisher in the database"
-  [newPublisher] (insert publisher (values newPublisher)))
+  ([newPublisher] 
+    (log/debug "insert publisher [" newPublisher "] into database")
+    (insert publisher (values newPublisher))))
 
 ; -------------------------------------------------------------------------------------
 ; DATABASE UPDATE
 
 (defn updateTitle
   "Updates a title in the database"
-  [titleMap] (update title 
-                     (set-fields {:isbn (get titleMap :isbn) 
-                                  :name (get titleMap :name)
-                                  :author (get titleMap :author)
-                                  :publisher_id (get titleMap :publisher_id)})
-                     (where {:id [= (get titleMap :id)]})))
+  [titleMap] 
+  (log/debug "update title [" titleMap "]")
+  (update title 
+          (set-fields {:isbn (get titleMap :isbn) 
+                       :name (get titleMap :name)
+                       :author (get titleMap :author)
+                       :publisher_id (get titleMap :publisher_id)})
+          (where {:id [= (get titleMap :id)]})))
 
 (defn updatePublisher
   "Updates a publisher in the database"
-  [publisherMap] (update publisher
-                         (set-fields {:name (get publisherMap :name)})                        
-                         (where {:id [= (get publisherMap :id)]})))
+  [publisherMap] 
+  (log/debug "update publisher [" publisherMap "]")
+  (update publisher
+          (set-fields {:name (get publisherMap :name)})                        
+          (where {:id [= (get publisherMap :id)]})))
 
 ; -------------------------------------------------------------------------------------
 ; DATABASE DELETE
 
 (defn deleteTitle
   "Deletes a title in the database"
-  [titleId] (delete title 
-                    (where {:id [= titleId]})))
+  ([titleId] 
+    (log/debug "delete title with id [" titleId "]")
+    (delete title (where {:id [= titleId]}))))
 
 (defn deletePublisher
   "Deletes a publisher in the database"
-  [publisherId] (delete publisher 
-                        (where {:id [= publisherId]})))
+  ([publisherId] 
+    (log/debug "delete publisher with id [" publisherId "]")
+    (delete publisher (where {:id [= publisherId]}))))
